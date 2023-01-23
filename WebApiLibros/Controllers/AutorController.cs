0d4925f0893726be6641bd94ea13b7ce36f6d659
Franco Goyena <gfrancomariano@yahoo.com.ar>
2023-01-23 12:45:28 -0300

    Agregar archivos de proyecto.

---
+++ WebApiLibros/Controllers/AutorController.cs
@@ -0,0 +1,96 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using WebApiLibros.Data;//agregar
+using WebApiLibros.Models;
+
+namespace WebApiLibros.Controllers
+{
+    //api/autor
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AutorController : ControllerBase
+    {
+        //inyeccion de dependencia----inicia
+        //propiedad
+        private readonly DBLibrosBootcampContext context;
+        //constructor
+        public AutorController(DBLibrosBootcampContext context)
+        {
+            this.context = context;
+        }
+
+        //inyeccion de dependencia----fin
+
+
+        //GET: api/autor
+        [HttpGet]
+        public ActionResult<IEnumerable<Autor>> Get()
+        {
+            return context.Autores.ToList();
+        }
+        //GET api/autor/5
+        [HttpGet("{id}")]
+        public ActionResult<Autor>GetById(int id)
+        {
+            Autor autor = (from a in context.Autores
+                           where a.IdAutor == id
+                           select a).SingleOrDefault();
+            return autor;
+        }
+
+        //GET por edad api/autor/33
+        [HttpGet("listado/{edad}")]
+        public ActionResult<IEnumerable<Autor>> GetEdad(int edad)
+        {
+            List<Autor> autores = (from a in context.Autores
+                                     where a.Edad == edad
+                                     select a).ToList();
+            return autores;
+        }
+        //POST api/autor
+        [HttpPost]
+        public ActionResult Post(Autor autor)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            context.Autores.Add(autor);
+            context.SaveChanges();
+            return Ok();
+        }
+
+        //UPDATE
+        //PUT api/autor/2
+        [HttpPut("{id}")]
+        public ActionResult Put(int id, [FromBody]Autor autor)
+        {
+            if (id != autor.IdAutor)
+            {
+                return BadRequest();
+            }
+            context.Entry(autor).State = EntityState.Modified;
+            context.SaveChanges();
+            return Ok();
+        }
+        //DELETE api/autor/1
+        [HttpDelete("{id}")]
+        public ActionResult<Autor> Delete(int id)
+        {
+            var autor = (from a in context.Autores
+                         where a.IdAutor == id
+                         select a).SingleOrDefault();
+            if (autor == null)
+            {
+                return NotFound();
+            }
+            context.Autores.Remove(autor);
+            context.SaveChanges();
+            return autor;
+        }
+
+    }
+}
---
+++ WebApiLibros/Controllers/LibroController.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using WebApiLibros.Data;
+using WebApiLibros.Models;
+
+namespace WebApiLibros.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LibroController : ControllerBase
+    {
+        private readonly DBLibrosBootcampContext context;
+        public LibroController(DBLibrosBootcampContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Libro>> Get()
+        {
+            return context.Libros.ToList();
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<IEnumerable<Libro>> GetByIdTodos(int id)
+        {
+            List<Libro> libro = (from a in context.Libros
+                           where a.AutorId == id
+                           select a).ToList();
+            return libro;
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Libro> GetById(int id)
+        {
+            Libro libro = (from a in context.Libros
+                           where a.AutorId == id
+                           select a).SingleOrDefault();
+            return libro;
+        }
+
+        [HttpPost]
+        public ActionResult Post(Libro libro)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            context.Libros.Add(libro);
+            context.SaveChanges();
+            return Ok();
+        }
+
+        //UPDATE
+        //PUT api/autor/2
+        [HttpPut("{id}")]
+        public ActionResult Put(int id, [FromBody] Libro libro)
+        {
+            if (id != libro.AutorId)
+            {
+                return BadRequest();
+            }
+            context.Entry(libro).State = EntityState.Modified;
+            context.SaveChanges();
+            return Ok();
+        }
+        //DELETE api/autor/1
+        [HttpDelete("{id}")]
+        public ActionResult<Libro> Delete(int id)
+        {
+            var libro = (from a in context.Libros
+                         where a.AutorId == id
+                         select a).SingleOrDefault();
+            if (libro == null)
+            {
+                return NotFound();
+            }
+            context.Libros.Remove(libro);
+            context.SaveChanges();
+            return libro;
+        }
+
+    }
+}
